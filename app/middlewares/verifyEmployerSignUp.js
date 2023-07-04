e723c0d22043aee15f783e686449a5209f08154f
Lulama Komana <komanalulama20@gmail.com>
2023-07-04 15:37:10 +0200

    New API updates

--- app/middlewares/verifyEmployerSignUp.js
+++ app/middlewares/verifyEmployerSignUp.js
@@ -13,7 +13,7 @@ const checkForCompanyDuplicates = async (req, res, next) => {
       // Check duplicate email
       let emp_email = await employer.findOne({ company_email: req.body.company_email }).exec();
       if (emp_email) {
-        return res.status(400).send({ message: "Failed! Email is already in use!" });
+        return res.status(400).send({ message: "Failed! Email is already exist" });
       }
   
       next();
